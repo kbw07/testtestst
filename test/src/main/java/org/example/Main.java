5294f8de506f13a5ed5deadca236033fa11b8d92
Your Name <kbwook96@naver.com>
2025-09-10 11:13:22 +0900

    testsetsetsetset

--- test/src/main/java/org/example/Main.java
+++ test/src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
