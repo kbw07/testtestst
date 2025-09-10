d172bae600b333973a07cd5b8c33ea08958c4569
Your Name <kbwook96@naver.com>
2025-09-10 11:41:10 +0900

    sdfsdfk

--- test/src/main/java/org/example/Main.java
+++ test/src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// click the <icon src="AllIcons.Actions.Execute"/>icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"sdfsdf/>icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and sdf!");
