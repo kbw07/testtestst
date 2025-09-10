43028ad52883e88ddf287b45c460d4980f8b9332
Your Name <kbwook96@naver.com>
2025-09-10 11:22:49 +0900

    last

--- test/src/main/java/org/example/Main.java
+++ test/src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
